96adaa64e01a23c3f4876014951e2188952b2aca
Tz <853325224@qq.com>
2017-09-19 16:15:59 +0800

    初始化项目

---
+++ app/src/main/java/com/example/administrator/loveclassdemo/ui/BaseActivity.java
@@ -0,0 +1,33 @@
+package com.example.administrator.loveclassdemo.ui;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 封装公共的功能
+ * Created by Tz on 2017/9/19.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+        init();
+    }
+
+    /**
+     * 初始化方法，初始化功能，子类也可以覆写，实现自己的初始化
+     */
+    protected void init() {
+
+    }
+
+    /**
+     *子类必须实现这个方法，返回Activity的布局
+     * @return
+     */
+    public abstract int getLayoutResId();
+}
---
+++ app/src/main/java/com/example/administrator/loveclassdemo/ui/BaseFragment.java
@@ -0,0 +1,37 @@
+package com.example.administrator.loveclassdemo.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Tz on 2017/9/19.
+ */
+
+public abstract class BaseFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(getLayoutResId(), null);
+        init();
+        return root;
+    }
+
+    /**
+     * 初始化方法，初始化功能，子类也可以覆写，实现自己的初始化
+     */
+    protected void init() {
+
+    }
+
+    /**
+     *子类必须实现这个方法，返回Activity的布局
+     * @return
+     */
+    public abstract int getLayoutResId();
+
+
+}
