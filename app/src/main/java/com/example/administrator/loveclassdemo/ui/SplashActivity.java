8fb43cc08fc7beca26927702cf55e64792e12904
Tz <853325224@qq.com>
2017-09-25 10:42:47 +0800

    Splash界面的实现

---
+++ app/src/main/java/com/example/administrator/loveclassdemo/ui/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.example.administrator.loveclassdemo.ui;
+
+import com.example.administrator.loveclassdemo.R;
+
+/**
+ * Created by Tz on 2017/9/19.
+ */
+
+public class SplashActivity extends BaseActivity {
+    @Override
+    public int getLayoutResId() {
+        return R.layout.activity_splash;
+    }
+}
