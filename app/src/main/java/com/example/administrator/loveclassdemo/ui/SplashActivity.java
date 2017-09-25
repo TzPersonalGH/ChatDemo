5fb6269f93db1d3d3b46fc2caa5586107bd8fc4b
Tz <853325224@qq.com>
2017-09-25 11:25:17 +0800

    根据登录状态进行界面跳转

--- app/src/main/java/com/example/administrator/loveclassdemo/ui/SplashActivity.java
+++ app/src/main/java/com/example/administrator/loveclassdemo/ui/SplashActivity.java
@@ -1,5 +1,9 @@
 package com.example.administrator.loveclassdemo.ui;
 
+import android.content.Intent;
+import android.os.Handler;
+
+import com.example.administrator.loveclassdemo.MainActivity;
 import com.example.administrator.loveclassdemo.R;
 
 /**
@@ -7,8 +11,54 @@ import com.example.administrator.loveclassdemo.R;
  */
 
 public class SplashActivity extends BaseActivity {
+    private Handler mHandler = new Handler();
+    private static final int DELAY = 2000;
+
     @Override
     public int getLayoutResId() {
         return R.layout.activity_splash;
     }
+
+    protected void init(){
+        super.init();
+        //检查登录状态
+        if (checkLoginStatus()) {
+            //已经登录则跳转到主界面
+            navigateToMain();
+        } else {
+            //没有登录则延迟两秒，跳转到登录界面
+            navigateToLogin();
+        }
+
+
+    }
+
+    /**
+     * 没登陆，跳转到登录界面
+     */
+    private void navigateToLogin() {
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },DELAY);
+    }
+
+    private boolean checkLoginStatus() {
+        return false;
+    }
+
+    /**
+     * 已登录，跳转到主界面
+     */
+    private void navigateToMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
 }
