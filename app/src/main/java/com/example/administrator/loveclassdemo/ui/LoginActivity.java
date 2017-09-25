83388d7983ca78ce8526bd115ec3104af7b3afdc
Tz <853325224@qq.com>
2017-09-25 17:28:49 +0800

    黄油刀的集成和跳转到注册界面

--- app/src/main/java/com/example/administrator/loveclassdemo/ui/LoginActivity.java
+++ app/src/main/java/com/example/administrator/loveclassdemo/ui/LoginActivity.java
@@ -1,13 +1,43 @@
 package com.example.administrator.loveclassdemo.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import com.example.administrator.loveclassdemo.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Tz on 2017/9/25.
  */
-public class LoginActivity extends BaseActivity{
+public class LoginActivity extends BaseActivity {
+    @BindView(R.id.user)
+    EditText mUser;
+    @BindView(R.id.password)
+    EditText mPassword;
+    @BindView(R.id.login)
+    Button mLogin;
+    @BindView(R.id.new_user)
+    TextView mNewUser;
+
     @Override
     public int getLayoutResId() {
         return R.layout.activity_login;
+    }
+
+    public void init() {
+        ButterKnife.bind(this);
+    }
+
+
+    @OnClick(R.id.new_user)
+    public void onClick() {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/administrator/loveclassdemo/ui/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.loveclassdemo.ui;
+
+import com.example.administrator.loveclassdemo.R;
+
+/**
+ * Created by Tz on 2017/9/25.
+ */
+public class RegisterActivity extends BaseActivity{
+    @Override
+    public int getLayoutResId() {
+        return R.layout.activity_register;
+    }
+}
