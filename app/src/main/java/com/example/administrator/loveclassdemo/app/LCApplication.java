18f536de863f72597f3cd09e4b2c59258059e078
Tz <853325224@qq.com>
2017-09-26 18:35:45 +0800

    环信初始化

--- app/src/main/java/com/example/administrator/loveclassdemo/app/LCApplication.java
+++ app/src/main/java/com/example/administrator/loveclassdemo/app/LCApplication.java
@@ -2,13 +2,17 @@ package com.example.administrator.loveclassdemo.app;
 
 import android.app.Application;
 
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMOptions;
+
 import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BuildConfig;
 
 /**
  * Created by Tz on 2017/9/26.
  */
 
-public class QQApplication extends Application{
+public class LCApplication extends Application{
 
     /**
      *当应用进程启动时，会调用onCreate方法
@@ -22,6 +26,25 @@ public class QQApplication extends Application{
         //getApplicationContext获取application的上下文（context）
         //它的生命周期要比activity的上下文要长
         Bmob.initialize(getApplicationContext(),"2aee7a758b4a55b85ceded9351a11c17");
+
+        initEaseMob();
+
+    }
+
+    private void initEaseMob() {
+        //创建环信配置
+        EMOptions options = new EMOptions();
+        //是否默认接受好友请求
+        options.setAcceptInvitationAlways(true);
+
+        //初始化
+        EMClient.getInstance().init(getApplicationContext(), options);
+
+        if (BuildConfig.DEBUG) {
+            EMClient.getInstance().setDebugMode(true);
+        }
+
+
 
     }
 }
