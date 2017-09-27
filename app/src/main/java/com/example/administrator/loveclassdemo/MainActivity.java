a7da457dd536ba68960c0af49644d0de2ce1c249
Tz <853325224@qq.com>
2017-09-27 20:30:42 +0800

    Bottombar的集成

--- app/src/main/java/com/example/administrator/loveclassdemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/loveclassdemo/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.administrator.loveclassdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.administrator.loveclassdemo.ui.BaseActivity;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends BaseActivity {
+
+
+    @BindView(R.id.bottomBar)
+    BottomBar mBottomBar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int getLayoutResId() {
+        return R.layout.activity_main;
     }
+
+    @Override
+    protected void init() {
+        super.init();
+        mBottomBar.setOnTabSelectListener(mOnTabSelectListener);
+    }
+
+    private OnTabSelectListener mOnTabSelectListener = new OnTabSelectListener() {
+        @Override
+        public void onTabSelected(@IdRes int tabId) {
+
+        }
+    };
+
 }
